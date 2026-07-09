7f55b4b0b85761a35cc383c985e40ad39c7cfbca
Sakthi Nivethitha  G <sakthinivethitha.g1@gmail.com>
2026-07-09 10:06:49 +0530

    Create 1446-AngleBetweenHandsOfAClock

---
+++ 1446-AngleBetweenHandsOfAClock/1446-AngleBetweenHandsOfAClock.java
@@ -0,0 +1,10 @@
+// Last updated: 7/9/2026, 10:06:49 AM
+class Solution {
+    public double angleClock(int hour, int minutes) {
+        double minuteAngle=minutes*6.0;
+        double hourAngle=(hour%12)*30.0+minutes*0.5;
+        double diff=Math.abs(hourAngle-minuteAngle);
+        return Math.min(diff,360.0-diff);
+        
+    }
+}
