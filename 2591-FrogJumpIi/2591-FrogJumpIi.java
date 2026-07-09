81f241e70d769661edac901d224ea494b59ddadc
Sakthi Nivethitha  G <sakthinivethitha.g1@gmail.com>
2026-07-09 10:06:45 +0530

    Create 2591-FrogJumpIi

---
+++ 2591-FrogJumpIi/2591-FrogJumpIi.java
@@ -0,0 +1,13 @@
+// Last updated: 7/9/2026, 10:06:44 AM
+class Solution {
+    public int maxJump(int[] stones) {
+
+        int ans = stones[1] - stones[0];
+
+        for (int i = 2; i < stones.length; i++) {
+            ans = Math.max(ans, stones[i] - stones[i - 2]);
+        }
+
+        return ans;
+    }
+}
