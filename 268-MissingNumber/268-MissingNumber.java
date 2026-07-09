5af58c44e09e338ca97853be5ff8f4f038437670
Sakthi Nivethitha  G <sakthinivethitha.g1@gmail.com>
2026-07-09 10:07:02 +0530

    Create 268-MissingNumber

---
+++ 268-MissingNumber/268-MissingNumber.java
@@ -0,0 +1,15 @@
+// Last updated: 7/9/2026, 10:07:02 AM
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+
+        int expectedSum = n * (n + 1) / 2;
+        int actualSum = 0;
+
+        for (int num : nums) {
+            actualSum += num;
+        }
+
+        return expectedSum - actualSum;
+    }
+}
