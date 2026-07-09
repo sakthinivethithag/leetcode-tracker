317199f49f7779a1097d44d0155ea6d45d6b8557
Sakthi Nivethitha  G <sakthinivethitha.g1@gmail.com>
2026-07-09 10:08:16 +0530

    Create 13-RomanToInteger

---
+++ 13-RomanToInteger/13-RomanToInteger.java
@@ -0,0 +1,31 @@
+// Last updated: 7/9/2026, 10:08:15 AM
+class Solution {
+    public int romanToInt(String s) {
+        int result = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            int current = value(s.charAt(i));
+
+            if (i < s.length() - 1 && current < value(s.charAt(i + 1))) {
+                result -= current;
+            } else {
+                result += current;
+            }
+        }
+
+        return result;
+    }
+
+    public int value(char ch) {
+        switch (ch) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+            default: return 0;
+        }
+    }
+}
